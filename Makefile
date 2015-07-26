803b61b06169262f772ded4af12ff7d71eb0fe26
ChronoAnomaly <chronoanomaly@gmail.com>
2015-07-26 19:04:14 -0400

    started basic files

---
+++ Makefile
@@ -0,0 +1,5 @@
+CFLAGS = -std=c89 -pedantic -Wall
+
+
+project:
+	gcc -o Worley-project3 $(CFLAGS) worley_project3.c
---
+++ worley_project3.c
@@ -0,0 +1,14 @@
+/*
+ * Brett Worley
+ * CEG-4350-C01
+ *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+}
