b1a89746593f48398a429ab84592960d4de81a95
ChronoAnomaly <chronoanomaly@gmail.com>
2015-07-27 18:45:35 -0400

    added ascii to hex function

--- worley_project3.c
+++ worley_project3.c
@@ -6,20 +6,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* max size of the memory space for the y86 emulator */
 #define MEMSIZE (int)pow(2, 32)
 
+/*######################################
+ * Function define space
+ *######################################
+*/
+
+int ascii_to_hex(char c);
+
+/*######################################
+ * Global variable space
+ *######################################
+*/
+
 unsigned int pc;	/* program counter */
 unsigned int stack_limit;	/* smallest address that the stack can not go past */
 
+unsigned char* memory;
+
 int reg[8];
+
+typedef enum {AOK = 1, HLT, ADR, INS, STC} status;
 
 int main(int argc, char** argv)
 {
 
 	FILE* fp;
+	
+	memory = (unsigned char*)malloc(sizeof(unsigned char) * MEMSIZE);
 
+	if(memory != NULL) {
+		printf("Valid memory pointer.\n");
+	}
 
 	if(argc < 2) {
 		printf("Not enough arguments for the program to run correctly\n.");
@@ -32,9 +54,34 @@ int main(int argc, char** argv)
 	}
 
 	if( (fp = fopen(argv[1], "r")) == NULL) {
-		printf("ERROR: The file you provided does not exisit.\n");
+		printf("ERROR: The file you provided does not exist.\n");
 		return EXIT_FAILURE;
 	}
 
+
+
+	while() {
+
+	}
+	fclose(fp);
+	free(memory);
+
 	return EXIT_SUCCESS;
+}
+
+
+int ascii_to_hex(char c)
+{
+
+	int nugm = (int)c;
+	
+	if(num < 58 && num > 47) {
+		return num - 48;
+	}
+
+	if(num < 103 && num > 96) {
+		return num - 87;
+	}
+	
+	return num;
 }
